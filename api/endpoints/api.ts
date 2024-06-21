1f22e79b390e3ed4fdd1419c0c804e26ca4754b8
Santin11fck <nikolaj.hansen.ri@gmail.com>
2024-06-21 13:11:08 -0400

    Corrección en api.ts
    Correción en archivo api.ts

--- api/endpoints/api.ts
+++ api/endpoints/api.ts
@@ -2,10 +2,14 @@ import { Elysia } from 'elysia';
 import registrarUsuario from './registrarUsuario';
 import obtenerInformacionUsuario from './obtenerInformacionUsuario';
 import bloquearUsuario from './bloquearUsuario';
+import marcarCorreoFavorito from './marcarCorreoFavorito';
+import desmarcarCorreoFavorito from './desmarcarCorreoFavorito';
 
 const app = new Elysia();
 
 app.use(registrarUsuario);
 app.use(obtenerInformacionUsuario);
 app.use(bloquearUsuario);
+app.use(marcarCorreoFavorito);
+app.use(desmarcarCorreoFavorito);
 export default app;
